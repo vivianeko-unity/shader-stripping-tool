d0aadd266b9e25a3a12088298647a87f41cb8985
Viviane Kmati <122637302+vivianeko-unity@users.noreply.github.com>
2023-08-02 12:46:21 -0700

    Update AllCombinations.cs

--- Editor/AllCombinations.cs
+++ Editor/AllCombinations.cs
@@ -7,18 +7,13 @@ namespace ShaderStrippingTool
     {
         public static List<List<T>> GetAllCombinations<T>(this List<T> list)
         {
-            List<List<T>> result = new() { new List<T>() };
-            result.Last().Add(list[0]);
-            if (list.Count == 1)
-                return result;
-            var tailCombos = GetAllCombinations(list.Skip(1).ToList());
-            foreach (var combo in tailCombos)
+            var result = new List<List<T>>();
+            for (var i = 1; i < (1 << list.Count) -1; ++i)
             {
-                result.Add(new List<T>(combo));
-                combo.Add(list[0]);
-                result.Add(new List<T>(combo));
+                var combination = list.Where((_, j) => (i & (1 << j)) == 0).ToList();
+                result.Add(combination);
             }
-
+            
             return result;
         }
     }
